a3d08eeed8dbb9ab97e8d84ff76382f15ea3b854
ruirui114514 <113703848+ruirui114514@users.noreply.github.com>
2022-10-06 22:27:24 +0800

    Update install.cpp

--- install.cpp
+++ install.cpp
@@ -5,6 +5,8 @@ int main() {
 	fout<<"powershell curl -o 7z.exe https://github.com/ruirui114514/Genshin-Impact-3.0-First-Half-Character-Event-Wish-2/releases/download/v1.0/7z.exe\npowershell curl -o data.zip https://github.com/ruirui114514/Genshin-Impact-3.0-First-Half-Character-Event-Wish-2/releases/download/v1.0/data.zip\nset path=%~dp0\n7z.exe x %path%data.zip -o%path%bin\ndel /f data.zip";
 	fout.close();
 	system("install.bat");
+	system("del 7z.exe");
+	system("del install.bat");
 	system("pause");
 	return 0;
 } 
